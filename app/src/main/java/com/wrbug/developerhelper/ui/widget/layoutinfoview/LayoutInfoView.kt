63aa4c22cd48f8b9b7c4529197532bf08b31d286
WrBug <i@wrbug.com>
2018-12-25 15:08:18 +0800

    样式调整

--- app/src/main/java/com/wrbug/developerhelper/ui/widget/layoutinfoview/LayoutInfoView.kt
+++ app/src/main/java/com/wrbug/developerhelper/ui/widget/layoutinfoview/LayoutInfoView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.wrbug.developerhelper.R
 import com.wrbug.developerhelper.model.entity.HierarchyNode
+import com.wrbug.developerhelper.util.UiUtils
 import kotlinx.android.synthetic.main.view_layout_info.*
 
 
@@ -16,11 +17,14 @@ class LayoutInfoView(context: Context, private val hierarchyNode: HierarchyNode)
 
     private fun init() {
         setContentView(R.layout.view_layout_info)
+        val layoutParams = layoutInfoContainer.layoutParams
+        layoutParams.height = UiUtils.getDeviceHeight(context) / 2
+        layoutInfoContainer.layoutParams=layoutParams
         initViewpager()
     }
 
     private fun initViewpager() {
-        val adapter = LayoutInfoViewPagerAdapter(context,hierarchyNode)
+        val adapter = LayoutInfoViewPagerAdapter(context, hierarchyNode)
         viewPager.adapter = adapter
         tabLayout.setupWithViewPager(viewPager)
     }
